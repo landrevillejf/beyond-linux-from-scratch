47f8c8d4860565151d91657bdd6af129143d6fb3
Jean-Francois Landreville <landrevillejf@protonmail.com>
2026-08-12 14:08:02 -0400

    fix

--- lfs/05-build-lfs-system.sh
+++ lfs/05-build-lfs-system.sh
@@ -39,10 +39,10 @@ if [ -z "$LFS" ]; then
     exit 1
 fi
 
-if [ -d "$LFS/image/tools" ] && [ -d "$LFS/image/usr" ] && [ ! -d "$LFS/tools" ]; then
-    log_warning "Detected image-root layout at $LFS/image, switching LFS root"
-    LFS="$LFS/image"
-fi
+#if [ -d "$LFS/image/tools" ] && [ -d "$LFS/image/usr" ] && [ ! -d "$LFS/tools" ]; then
+#    log_warning "Detected image-root layout at $LFS/image, switching LFS root"
+#    LFS="$LFS/image"
+#fi
 
 run_privileged() {
     if [ "$(whoami)" = "root" ]; then
